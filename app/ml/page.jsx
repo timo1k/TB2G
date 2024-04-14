48376225dceb0236f42a65448238d9dbca530f9b
Timothy <timkhumpan@gmail.com>
2024-04-14 10:10:40 -0400

    ml

---
+++ app/ml/page.jsx
@@ -0,0 +1,26 @@
+"use client"
+import React, { useState, useEffect } from "react";
+
+const Searched = ({ params }) => {
+
+
+return (
+    <div className="min-w-full max-w-full min-h-full h-screen
+    bg-white
+    p-5">
+      <div className="flex flex-col items-center
+      w-full
+      text-center
+      font-bold text-6xl justify-center bg-white">
+        TB2G
+      </div>
+
+      <div className="mx-10 sm:mx-20 md:mx-40 lg:mx-60 xl:mx-80">
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Searched;
--- app/page.js
+++ app/page.js
@@ -38,7 +38,6 @@ export default function Home() {
 
   return (
     <main className="min-w-full max-w-full min-h-full h-screen bg-white">
-
       <div className="flex items-center justify-between w-full p-5 text-center font-bold text-6xl">
         <Link href={"/insert"}>
           <Button>CREATE ME</Button>
@@ -50,72 +49,76 @@ export default function Home() {
       </div>
 
       <Marquee
-          items={[
-            "Recently View",
-            "Recently View",
-            "Recently View",
-            "Recently View",
-            "Recently View",
-            "Recently View",
-            "Recently View",
-          ]}/>
+        items={[
+          "Recently View",
+          "Recently View",
+          "Recently View",
+          "Recently View",
+          "Recently View",
+          "Recently View",
+          "Recently View",
+        ]}
+      />
 
-      <div className=" flex flex-col justify-center text-center
-        items-center m-10">
-
+      <div
+        className=" flex flex-col justify-center text-center
+        items-center m-10"
+      >
         <Link href={"/searched"}>
-          <Button>
-            I Need to SHIT FAST
-          </Button>
+          <Button>I Need to SHIT FAST</Button>
+        </Link>
+        <br></br>
+
+        <Link href={"/ml"}>
+          <Button>Have the Best Shit!</Button>
         </Link>
 
-          <div className="flex justify-center text-center items-center mx-10 sm:mx-20 md:mx-40 lg:mx-60 xl:mx-80">
-            <Link href={"/Wachman"}>
-              <div className="mr-4 mt-6">
-                <ImageCard
-                  imageUrl="https://live.staticflickr.com/3679/12726907403_b2182559c8_b.jpg"
-                  children={<div>Wachman</div>}
-                />
-              </div>
-            </Link>
-            <Link href={"/Tuttleman"}>
-              <div className="mr-4 mt-6">
-                <ImageCard
-                  imageUrl="https://pbs.twimg.com/media/EyFEwFdXAAYSuhr.jpg:large"
-                  children={<div>Tuttleman</div>}
-                />
-              </div>
-            </Link>
-            <Link href={"/SERC"}>
-              <div className="mr-4 mt-6">
-                <ImageCard
-                  imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt0k9SKvgfsoRgGrMYjB8SuU_MDEf3BUjA0Yv_rS0utg&s"
-                  children={<div>SERC</div>}
-                />
-              </div>
-            </Link>
-          </div>
-          <br></br>
+        <div className="flex justify-center text-center items-center mx-10 sm:mx-20 md:mx-40 lg:mx-60 xl:mx-80">
+          <Link href={"/Wachman"}>
+            <div className="mr-4 mt-6">
+              <ImageCard
+                imageUrl="https://live.staticflickr.com/3679/12726907403_b2182559c8_b.jpg"
+                children={<div>Wachman</div>}
+              />
+            </div>
+          </Link>
+          <Link href={"/Tuttleman"}>
+            <div className="mr-4 mt-6">
+              <ImageCard
+                imageUrl="https://pbs.twimg.com/media/EyFEwFdXAAYSuhr.jpg:large"
+                children={<div>Tuttleman</div>}
+              />
+            </div>
+          </Link>
+          <Link href={"/SERC"}>
+            <div className="mr-4 mt-6">
+              <ImageCard
+                imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt0k9SKvgfsoRgGrMYjB8SuU_MDEf3BUjA0Yv_rS0utg&s"
+                children={<div>SERC</div>}
+              />
+            </div>
+          </Link>
+        </div>
+        <br></br>
 
-          <h2 className="font-bold">Reviews</h2>
+        <h2 className="font-bold">Reviews</h2>
 
-          <BuildingInfo data={users} />
+        <BuildingInfo data={users} />
       </div>
-
     </main>
   );
 }
 
 const BuildingInfo = ({ data }) => {
-
   const rating = (item) => {
-
-    let sum =(parseInt(item.amenities) +
-                  parseInt(item.cleanliness) +
-                  parseInt(item.privacy)) / 3
+    let sum =
+      (parseInt(item.amenities) +
+        parseInt(item.cleanliness) +
+        parseInt(item.privacy)) /
+      3;
 
     return Math.round(sum * 10) / 10;
-  }
+  };
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
@@ -131,8 +134,7 @@ const BuildingInfo = ({ data }) => {
                 <strong> Floor:</strong> {item.floor}
               </span>
               <span>
-                <strong> Rating:</strong>{" "}
-                {rating(item)}
+                <strong> Rating:</strong> {rating(item)}
               </span>
             </figcaption>
           </figure>
