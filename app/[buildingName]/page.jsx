0574fa4331f639a55b32cb6543aed279cf0d9767
bryanli200282 <bryanli200282@gmail.com>
2024-04-14 00:29:44 -0400

    fix merge conflict

--- app/[buildingName]/page.jsx
+++ app/[buildingName]/page.jsx
@@ -1,13 +1,13 @@
-"use client";
+"use client"
 import React, { useState, useEffect } from "react";
 import Accordion from "../_components/Accordion";
+import Rating from "../_components/Rating"; // Import the Rating component
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 import { orderBy } from "firebase/firestore";
 import { query } from "firebase/firestore";
-import { BarChart } from "@mui/x-charts/BarChart";
 
-const building = ({ params }) => {
+const Building = ({ params }) => {
   // Accessing the id from params
   const id = params.buildingName;
 
@@ -90,16 +90,11 @@ const building = ({ params }) => {
 
   return (
     <div className="min-w-full max-w-full min-h-full h-screen bg-white border-black border-2">
-      <div
-        className="flex flex-col items-center w-full p-5 text-center
-        font-bold text-6xl justify-center bg-white"
-      >
+      <div className="flex flex-col items-center w-full p-5 text-center font-bold text-6xl justify-center bg-white">
         TB2G
       </div>
 
-      <h1 className="text-black text-3xl md:text-4xl lg:text-4xl xl:text-4xl pl-12 pt-7">
-        {id}
-      </h1>
+      <h1 className="text-black text-3xl md:text-4xl lg:text-4xl xl:text-4xl pl-12 pt-7">{id}</h1>
       <br></br>
       <div className="flex justify-center">
         {/* <BarChart
@@ -122,30 +117,15 @@ const building = ({ params }) => {
         /> */}
       </div>
       <br></br>
+  
       {/* floors to shit on */}
-      <div className="flex justify-center text-center items-center mx-10 sm:mx-20 md:mx-40 lg:mx-60 xl:mx-80 h-5">
+      <div className="flex justify-center text-center items-center mx-10 sm:mx-20 md:mx-40 lg:mx-60 xl:mx-80">
         <div className="mr-4 mt-6">
-          <Accordion question="1st floor" answer="get shit on" />
+          <Rating question="1st floor" answer="get shit on" />
         </div>
-        <h1 className="text-black text-3xl md:text-4xl lg:text-4xl xl:text-4xl pl-12 pt-7">
-            {id}
-        </h1>
-
-        {/* floors to shit on */}
-        <div className="flex justify-center text-center items-center mx-10 sm:mx-20 md:mx-40 lg:mx-60 xl:mx-80">
-            <div className="mr-4 mt-6">
-                <Rating question="1st floor" answer="get shit on" />
-            </div>
-            
-        </div>
-        
-        
-        
-        
+      </div>
     </div>
   );
 };
 
-
-export default building
-
+export default Building;
