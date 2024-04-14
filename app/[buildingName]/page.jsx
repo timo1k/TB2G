9ce298913374bef70cc4dc16a11672dce00526c9
Phuykong <m.phuykong@gmail.com>
2024-04-14 01:25:25 -0400

    change the page

--- app/[buildingName]/page.jsx
+++ app/[buildingName]/page.jsx
@@ -1,11 +1,11 @@
 "use client"
 import React, { useState, useEffect } from "react";
+import { collection, getDocs, db, orderBy, query } from "firebase/firestore";
+
+// components
 import Accordion from "../_components/Accordion";
 import Rating from "../_components/Rating"; // Import the Rating component
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
-import { orderBy } from "firebase/firestore";
-import { query } from "firebase/firestore";
+
 
 const Building = ({ params }) => {
   // Accessing the id from params
@@ -89,35 +89,28 @@ const Building = ({ params }) => {
   }, []);
 
   return (
-    <div className="min-w-full max-w-full min-h-full h-screen bg-white border-black border-2">
-      <div className="flex flex-col items-center w-full p-5 text-center font-bold text-6xl justify-center bg-white">
+    <div className="min-w-full max-w-full min-h-full h-screen
+    bg-white
+    p-5">
+      <div className="flex flex-col items-center
+      w-full
+      text-center
+      font-bold text-6xl justify-center bg-white">
         TB2G
       </div>
 
-      <h1 className="text-black text-3xl md:text-4xl lg:text-4xl xl:text-4xl pl-12 pt-7">{id}</h1>
-      <br></br>
-      <div className="flex justify-center">
-        {/* <BarChart
-          number="5"
-          layout="horizontal"
-          yAxis={[
-            {
-              id: "barCategories",
-              data: ["Cleanliness", "Amenities", "Privacy"],
-              scaleType: "band",
-            },
-          ]}
-          series={[
-            {
-              data: [cleanAvg, amenAvg, privacyAvg],
-            },
-          ]}
-          width={500}
-          height={300}
-        /> */}
+      <div className="">
+
+        <h1 className="text-black text-3xl md:text-4xl lg:text-4xl xl:text-4xl">'
+          {id}
+        </h1>
+
+        <div className="flex justify-center">
+
+        </div>
+
       </div>
-      <br></br>
-  
+
       {/* floors to shit on */}
       <div className="flex justify-center text-center items-center mx-10 sm:mx-20 md:mx-40 lg:mx-60 xl:mx-80">
         <div className="mr-4 mt-6">
